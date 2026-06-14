84519fdbfc88f63271e19b324d54a8e8a4a46690
sanusi <sanusi100201@gmail.com>
2026-06-14 21:34:04 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+new Typed("#typing", {
+strings:[
+"Web Developer",
+"UI/UX Designer",
+"Frontend Developer",
+"Freelancer"
+],
+typeSpeed:70,
+backSpeed:50,
+loop:true
+});
+
+particlesJS("particles-js", {
+particles: {
+number: {
+value: 80
+},
+size: {
+value: 3
+},
+move: {
+speed: 2
+}
+}
+});
